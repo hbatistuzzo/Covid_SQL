fa71ed719cbe33fd50bd0fd615f435f6ff2cfbd2
HBatistuzzo <hbatistuzzo@gmail.com>
2022-12-18 18:25:55 -0300

    sql

--- SQL_covid.sql
+++ SQL_covid.sql
@@ -9,8 +9,16 @@
 --FROM Covid..covid_deaths
 --order by 1,2 --just so it's in the same format as the original data we saw in Excel
 
--- Looking at Total Cases vs Total Deaths. What does the mortality look like?
-SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as 'death%'
-FROM Covid..covid_deaths
-WHERE location = 'Brazil'
-order by 1,2 
+---- Looking at Total Cases vs Total Deaths. What does the mortality look like?
+--SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as 'death%'
+--FROM Covid..covid_deaths
+--WHERE location = 'Brazil'
+--order by 1,2 
+
+---- Total Cases vs Population. What % got covid?
+--SELECT Location, date, total_cases, Population, (total_cases/Population)*100 as 'case%'
+--FROM Covid..covid_deaths
+--WHERE location = 'Brazil'
+--order by 1,2 
+
+
